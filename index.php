4c67687ed0cc862a76e7d5b573e9f55b04929b31
Enno Hyttrek <ehy.training@gmail.com>
2023-11-14 12:10:37 +0100

    Version: 1.1.0

--- index.php
+++ index.php
@@ -11,16 +11,11 @@
 <body>
 
     <?php
-            if ( have_posts() ) :
-                while ( have_posts() ) : the_post();
-    ?>
-
-
-    <?php the_content(); ?>
-
-    <?php
-                endwhile;
-            endif;
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                the_content(); 
+            endwhile;
+        endif;
     ?>
 
 </body>
